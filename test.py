455ec59a8effbf24253750cbc187f491c98ac00e
tro23 <trey.roessig@gmail.com>
2014-09-15 21:15:15 -0700

    Added coverage testing

--- test.py
+++ test.py
@@ -5,6 +5,29 @@ import unittest
 
 sys.path.append('test')
 sys.path.append('test/lib')
-from test import *
+import test
 
-unittest.main()
+
+try:
+    import coverage
+    covering = True
+except:
+    covering = False
+    print 'No coverage'
+
+
+if covering:
+    cov = coverage.coverage(branch=True,
+                            config_file = 'test/.coveragerc',
+                            data_file = 'test/coverage/.coverage'
+                            )
+    cov.start()
+
+loader = unittest.TestLoader()
+suite = unittest.TestSuite(loader.loadTestsFromModule(test))
+unittest.TextTestRunner(verbosity=1).run(suite)
+
+if covering:
+    cov.stop()
+    cov.save()
+    cov.html_report(directory='test/coverage')
